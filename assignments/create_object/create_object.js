d7c171dd68902d9b6667949165abb552e46fb467
sanchitanwar0007 <sanchitanwar0007@gmail.com>
2021-11-29 21:11:06 +0530

    Create_object_completed

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
+    map={};
+    for(var i=0;i<arr.length;i+=2){
+        map[arr[i]]=arr[i+1];
+    }
+    return map;
 }
 
 module.exports = CreateObject;
